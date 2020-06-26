dafc83475afd1a490652093ce299e71ecc346b56
Amr Farouk <amrfarouk0123@gmail.com>
2020-06-26 16:01:14 +0300

    SearchBy Controllers Implemented

--- src/Controller/SearchByCityController.php
+++ src/Controller/SearchByCityController.php
@@ -27,4 +27,5 @@ class SearchByCityController extends AbstractController
     }
 
 
+
 }
--- src/Controller/SearchByWeatherController.php
+++ src/Controller/SearchByWeatherController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Products;
 use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SearchByWeatherController extends AbstractController
@@ -12,17 +14,28 @@ class SearchByWeatherController extends AbstractController
 
     /**
      * @Route("/searchByWeather", name="searchByWeather")
+     * @return Response
+     */
+    public function searchByWeather()
+    {
+        return $this -> render('search_by_weather.html.twig');
+    }
+
+    /**
+     * @Route("/searchByWeather/weatherType/{type}", name="weatherTypeRoute")
+     * @param $type
      * @param ProductsRepository $productsRepository
      * @return Response
      */
-    public function searchByWeather(ProductsRepository $productsRepository)
+    public function index($type, ProductsRepository $productsRepository)
     {
 
-        $products = $productsRepository->findAll();
+        $products = $productsRepository->findBy(['weatherCondition' => $type], $limit = null, $offset = null);
 
-        return $this -> render('search_by_weather.html.twig', [
+        return $this -> render('show_by_weather.html.twig', [
             'products' => $products
         ]);
+
     }
 
 }
