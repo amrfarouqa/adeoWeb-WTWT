fcaa90bf20ded3bd0213da848eacf3692f56eb95
Amr Farouk <amrfarouk0123@gmail.com>
2020-06-26 15:59:36 +0300

    Search By Weather Implemented

--- src/Controller/SearchByWeatherController.php
+++ src/Controller/SearchByWeatherController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SearchByWeatherController extends AbstractController
@@ -10,10 +12,17 @@ class SearchByWeatherController extends AbstractController
 
     /**
      * @Route("/searchByWeather", name="searchByWeather")
+     * @param ProductsRepository $productsRepository
+     * @return Response
      */
-    public function searchByWeather()
+    public function searchByWeather(ProductsRepository $productsRepository)
     {
-        return $this -> render('search_by_weather.html.twig');
+
+        $products = $productsRepository->findAll();
+
+        return $this -> render('search_by_weather.html.twig', [
+            'products' => $products
+        ]);
     }
 
 }
---
+++ src/Entity/Products.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductsRepository::class)
+ */
+class Products
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=0)
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $img;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $productName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $bodyPart;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $weatherCondition;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getImg(): ?string
+    {
+        return $this->img;
+    }
+
+    public function setImg(string $img): self
+    {
+        $this->img = $img;
+
+        return $this;
+    }
+
+    public function getProductName(): ?string
+    {
+        return $this->productName;
+    }
+
+    public function setProductName(string $productName): self
+    {
+        $this->productName = $productName;
+
+        return $this;
+    }
+
+    public function getBodyPart(): ?string
+    {
+        return $this->bodyPart;
+    }
+
+    public function setBodyPart(string $bodyPart): self
+    {
+        $this->bodyPart = $bodyPart;
+
+        return $this;
+    }
+
+    public function getWeatherCondition(): ?string
+    {
+        return $this->weatherCondition;
+    }
+
+    public function setWeatherCondition(string $weatherCondition): self
+    {
+        $this->weatherCondition = $weatherCondition;
+
+        return $this;
+    }
+}
